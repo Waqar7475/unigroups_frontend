46bc97a9fddf5755cba553c5a4847def44ccf747
coder <wa985927@gmail.com>
2026-05-28 14:29:04 +0300

    mew

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  build: {
+    // Warning limit increase (default 500kb hota hai)
+    chunkSizeWarningLimit: 2000,
+
+    // Better chunk splitting
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
 })
